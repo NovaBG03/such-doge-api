0dcf5a99b285653194523542ef6881063631e36e
Nikita Koynov <nikicha03@gmail.com>
2021-09-28 12:50:14 +0300

    cors settings

--- src/main/java/xyz/suchdoge/webapi/security/WebSecurityConfig.java
+++ src/main/java/xyz/suchdoge/webapi/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package xyz.suchdoge.webapi.security;
 
+import com.google.common.collect.ImmutableList;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -10,6 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import xyz.suchdoge.webapi.security.jwt.JwtConfig;
 import xyz.suchdoge.webapi.security.jwt.JwtTokenVerifier;
 import xyz.suchdoge.webapi.security.jwt.JwtUserPasswordAuthenticationFilter;
@@ -18,16 +22,16 @@ import javax.crypto.SecretKey;
 
 @EnableWebSecurity
 @Configuration
-public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private final PasswordEncoder passwordEncoder;
     private final UserDetailsService userDetailsService;
     private final SecretKey secretKey;
     private final JwtConfig jwtConfig;
 
-    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder,
-                                     UserDetailsService userDetailsService,
-                                     SecretKey secretKey,
-                                     JwtConfig jwtConfig) {
+    public WebSecurityConfig(PasswordEncoder passwordEncoder,
+                             UserDetailsService userDetailsService,
+                             SecretKey secretKey,
+                             JwtConfig jwtConfig) {
         this.passwordEncoder = passwordEncoder;
         this.userDetailsService = userDetailsService;
         this.secretKey = secretKey;
@@ -39,8 +43,10 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .cors()
+                .and()
                 .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .addFilter(new JwtUserPasswordAuthenticationFilter(authenticationManager(), secretKey, jwtConfig))
                 .addFilterAfter(new JwtTokenVerifier(secretKey, jwtConfig), JwtUserPasswordAuthenticationFilter.class)
@@ -60,5 +66,17 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         provider.setPasswordEncoder(passwordEncoder);
         provider.setUserDetailsService(userDetailsService);
         return provider;
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(ImmutableList.of(CorsConfiguration.ALL));
+        configuration.setAllowedMethods(ImmutableList.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
